0533da9f86d9a9b6e6a98d87ebfcf575134a58f5
alex <alex@MacBook-Pro-Frank.local>
2020-07-08 21:58:57 -0400

    api working

---
+++ playlist_api/db/seeds.rb
@@ -0,0 +1,36 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Song.create([
+
+    {
+        "title": "One",
+        "artist_name": "U2"
+    },
+    {
+        "title": "Billie Jean",
+        "artist_name": "Michael Jackson"
+    },
+    {
+        "title": "Like A Rolling Stone",
+        "artist_name": "Bob Dylan"
+    },
+
+
+])
+
+
+Playlist.create([
+
+    {
+        "name": "The Top 50"
+    }
+
+
+])
