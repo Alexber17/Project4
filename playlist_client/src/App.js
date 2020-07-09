60360f60d6e679fb6074e52e7529079520d697aa
alex <alex@MacBook-Pro-Frank.local>
2020-07-08 22:28:05 -0400

    front end working

---
+++ playlist_client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Playlist from './components/Playlist.js'
+
+
+
+
+class App extends Component{
+  render(){
+    return(
+      <div className='App'>
+        <Playlist />
+      </div>
+    )
+  }
+}
+
+
+export default App;
