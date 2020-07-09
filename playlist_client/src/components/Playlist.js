781acd7cf1f083713ad614c89e443cc238d90398
alex <alex@MacBook-Pro-Frank.local>
2020-07-08 22:45:38 -0400

    react and rails connected

--- playlist_client/src/components/Playlist.js
+++ playlist_client/src/components/Playlist.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 
 
 class Playlist extends Component {
+
+    	
+    state = {
+        notices: []
+    }
+
+    componentDidMount() {
+        this.getNotices()
+    }
+
+
+    getNotices = () =>{
+        fetch('http://localhost:3000/songs')
+            .then(response => response.json())
+            .then(json => console.log(json))
+        .catch(error => console.error(error))
+    }
+
     render () {
       return (
         <h1>Playlist</h1>
