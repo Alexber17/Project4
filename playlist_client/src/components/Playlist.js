6be9e316c2e94a2c3e8864afb90bb7e3f4fa4a23
Alexander Bermudez <alexanderbermudez@Alexanders-MacBook-Pro.local>
2020-07-10 16:19:51 -0400

    add song completed

--- playlist_client/src/components/Playlist.js
+++ playlist_client/src/components/Playlist.js
@@ -11,6 +11,13 @@ class Playlist extends Component {
     state = {
         playlist: [],
         nameplay:'',
+
+        artist_name:'',
+        title:'',
+        url:'',
+        songPlay:'',
+        
+
         show:true
     }
     
@@ -32,7 +39,8 @@ class Playlist extends Component {
       event.preventDefault();
       fetch('http://localhost:3000/playlists',{
           body: JSON.stringify({
-          name:this.state.nameplay}),
+            name:this.state.nameplay
+          }),
           method:"POST",
           headers: {
               'Accept': 'application/json, text/plain, */*',
@@ -47,6 +55,32 @@ class Playlist extends Component {
         })
     })
     }
+    handleSubmitSong= event =>{
+      event.preventDefault();
+      fetch('http://localhost:3000/songs',{
+          body: JSON.stringify({
+          artist_name :this.state.nameplay,
+          title:this.state.title,
+          url:this.state.url
+         }),
+          method:"POST",
+          headers: {
+              'Accept': 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+            }
+      }).then(response=> response.json())
+      .then(newTodo=>{
+        this.setState({
+          // playlist:[newTodo, ...this.state.playlist],
+          artist_name:'',
+          title:'',
+          url:''
+
+        })
+    })
+    }
+
+    
 
 
 
@@ -68,23 +102,50 @@ class Playlist extends Component {
                     </div> 
                     <button type="submit" class="btn btn-primary"> Create </button>
             </form>
-
           </div>
-
-
           <div>
-            <h1>  Create Playlist </h1>
-            <form onSubmit={this.handleSubmit}>
+            <h1>  Add Song </h1>
+            <form onSubmit={this.handleSubmitSong}>
                     <div className="form-group row">
-                        <lable htmlFor='name' className='col-sm-1 col-form-label' >Description:</lable>
+                        <lable htmlFor='name' className='col-sm-1 col-form-label' >artist_name:</lable>
                         <div class="col-sm-11">
-                            <input type='text'  required value={this.state.nameplay} className='form-control' id='nameplay' onChange={this.handleChange}/>
+                            <input type='text'  required value={this.state.artist_name} className='form-control' id='artist_name' onChange={this.handleChange}/>
                         </div>
+
+                        <lable htmlFor='name' className='col-sm-1 col-form-label' >title:</lable>
+                        <div class="col-sm-11">
+                            <input type='text'  required value={this.state.title} className='form-control' id='title' onChange={this.handleChange}/>
+                        </div>
+                        <lable htmlFor='name' className='col-sm-1 col-form-label' >url:</lable>
+                        <div class="col-sm-11">
+                            <input type='text'  required value={this.state.url} className='form-control' id='url' onChange={this.handleChange}/>
+                        </div>
+
+                        <lable htmlFor='name' className='col-sm-1 col-form-label' >Playlist:</lable>
+                        <div class="col-sm-11">
+                       
+                            {/* <input    required  className='form-control'  onChange={this.handleChange}/> */}
+                            <input type='text'  required value={this.state.songPlay} className='form-control' id='songPlay' />
+                            <select type='text' id='songPlay' name="pets" onChange={this.handleChange}>
+                                <option value="">--Please choose an option--</option>
+                                {this.state.playlist.length > 0 && this.state.playlist.map((list, index) => {
+                                        return (
+                                        <>
+                                      <option value={list.id}>{list.name}</option>
+                                      </>
+                                        )
+                                    })}
+    
+                            </select>
+                        </div>
+
                     </div> 
                     <button type="submit" class="btn btn-primary"> Create </button>
             </form>
-
           </div>
+
+
+ 
 
            <h1>Playlist</h1>
           <ul>
@@ -92,6 +153,7 @@ class Playlist extends Component {
               return (
               <div>
               <button onClick={() => this.setState({ shown: !this.state.shown })} >{list.name}</button>
+              {/* <button onClick={() => thiisshow } >{list.name}</button> */}
               {this.state.shown ? <PlaylistSong item={list}/> : ''}  
                </div>
               )
--- playlist_client/src/components/PlaylistSong.js
+++ playlist_client/src/components/PlaylistSong.js
@@ -3,10 +3,24 @@ import React, { Component } from 'react';
 
 
 class PlaylistSong extends Component{
+    state = {
+
+        show:true
+    }
     render(){
         return(
            <div>
-                <h1>{this.props.item.id}</h1>
+                <h1>{this.props.item.name}</h1>
+
+                {this.props.item.songs.length > 0 && this.props.item.songs.map((song, index) => { 
+                  
+                    return(
+                        <div>
+                        <p>Artist:{song.artist_name}</p>
+                        <p>Title:{song.title}</p>
+                        </div>
+                    )
+                 })}
            </div>
            
            
