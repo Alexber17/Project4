33e59efb5917a82f88f94a05a17f341e06a7a74d
Alexander Bermudez <alexanderbermudez@Alexanders-MacBook-Pro.local>
2020-07-11 11:51:01 -0400

    saving

--- playlist_client/src/components/PlaylistSong.js
+++ playlist_client/src/components/PlaylistSong.js
@@ -10,6 +10,8 @@ class PlaylistSong extends Component{
     render(){
         return(
            <div>
+
+               
                 <h1>{this.props.item.name}</h1>
 
                 {this.props.item.songs.length > 0 && this.props.item.songs.map((song, index) => { 
